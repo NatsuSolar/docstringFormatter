1266066287d80fefa1194ab6a3ac875605d63fbe
NatsuSolar <72324129+NatsuSolar@users.noreply.github.com>
2023-08-05 22:12:07 +0900

    ### [Modify] Correct 2 files
    **Summary**
    1. (L.4,5) src/docstringFormatter/__init__.py
        - Delete: __version__
        - Modify: __license__: 'MIT' ---> 'The MIT License'
    
    2. (Other)  pyproject.toml
        - Correct some sentence
        - To finsh build at the point of GitHub releasing(Reference:  [Reference URL >> 3.](https://qiita.com/ieiringoo/items/4bef4fc9975803b08671#flat-layout-%E3%81%AE%E5%A0%B4%E5%90%88)
    )
    ***************************************************
    Commit [XXXXX](https://github.com)
    -- [Modify] Correct 2 files
    
    (Modify) src/docstringFormatter/__init__.py
    (Modify) pyproject.toml
    ***************************************************
    ### Reference URL
    1. [Configuring setuptools using_Setuptools-specific configuration](https://setuptools.pypa.io/en/latest/userguide/pyproject_config.html#setuptools-specific-configuration)
    
    2. [PyPIパッケージのリリースもバージョニングもGitHub単独で完結させる](https://zenn.dev/detsu/articles/5d74bf72e96a0f)
    
    3. [2022年版pyproject.tomlを使ったPythonパッケージの作り方](https://qiita.com/ieiringoo/items/4bef4fc9975803b08671#flat-layout-%E3%81%AE%E5%A0%B4%E5%90%88)
    
    #★☆☆☆☆
    #pyproject.toml #Repository secrets #API token #setuptools_scm #publish-to-pypi.yml
    #_version.py

--- src/docstringFormatter/__init__.py
+++ src/docstringFormatter/__init__.py
@@ -1,10 +1,11 @@
 from .docstringFormatter import *
 
 __copyright__    = 'Copyright (C) 2023 Aoi Natsuki'
-__version__      = '1.0.0'
-__license__      = 'MIT'
+__license__      = 'The MIT License'
 __author__       = 'Aoi Natsuki(蒼井 夏樹)'
 __author_email__ = 'blautinte8@gmail.com'
 __url__          = 'https://github.com/NatsuSolar/docstringFormatter'
+__CAT__          = "\n\n\Mew Mew Mew/ \n     ∧,∧\n     ΦωΦ\n    　🔔\n/RingRingRing\\"
+
 
 __all__ = ['docstringFormatter']
